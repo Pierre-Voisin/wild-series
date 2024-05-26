6bdf5478a1d40b6569f80af3f266f5812c5cce6e
Pierre-Voisin <voisin.pierre87@gmail.com>
2024-05-26 11:32:24 +0200

    modif

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ const app = require("./app/config");
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
 
+
 // Start the server and listen on the specified port
 app
   .listen(port, () => {
