7f40bbb3b9e6862af1ada7df5a41045054296ba6
Pierre-Voisin <voisin.pierre87@gmail.com>
2024-05-28 12:06:35 +0200

    createroute

---
+++ server/app/routers/api/programs/router.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+
+
+
+const router = express.Router();
+
+
+
+
+const {browse} = require("../../../controllers/programActions");
+
+
+
+
+router.get("/", browse);
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -1,14 +1,16 @@
 const express = require("express");
 
 const router = express.Router();
-
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
-
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
+
+const programsRouter = require("./programs/router");
+
+router.use("/programs", programsRouter);
 
 /* ************************************************************************* */
 
